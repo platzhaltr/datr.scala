44dbc6a641f57ab1e9c028371484ae8dd8cc9e53
Oliver Schrenk <oschrenk@elmar.nl>
2014-06-27 13:34:22 +0200

    Absolute month

--- src/main/scala/com/acme/DateParser.scala
+++ src/main/scala/com/acme/DateParser.scala
@@ -56,12 +56,15 @@ class DateParser(val input: ParserInput) extends Parser {
     RelativeFuture
   }
 
-  def RelativeFuture = rule { Next ~ Space ~ Weekday ~> ((w: Weekday) => Config(Some(w)) )}
+  def RelativeFuture = rule {
+    Next ~ Space ~ Weekday ~> ((w) => Config(weekday = Some(w))) |
+    Next ~ Space ~ Month   ~> ((m) => Config(month = Some(m)))
+  }
 
   def Next = rule { "next" }
 
+  // Absolute Weekday
   def Weekday: Rule1[Weekday]   = rule {Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday}
-
   def Monday: Rule1[Weekday]    = rule {capture(ignoreCase("monday")    | ignoreCase("mon") ) ~> ((s: String) => new Weekday(1) )}
   def Tuesday: Rule1[Weekday]   = rule {capture(ignoreCase("tuesday")   | ignoreCase("tue") ) ~> ((s: String) => new Weekday(2) )}
   def Wednesday: Rule1[Weekday] = rule {capture(ignoreCase("wednesday") | ignoreCase("wed") ) ~> ((s: String) => new Weekday(3) )}
@@ -70,6 +73,22 @@ class DateParser(val input: ParserInput) extends Parser {
   def Saturday: Rule1[Weekday]  = rule {capture(ignoreCase("saturday")  | ignoreCase("sat") ) ~> ((s: String) => new Weekday(6) )}
   def Sunday: Rule1[Weekday]    = rule {capture(ignoreCase("sunday")    | ignoreCase("sun") ) ~> ((s: String) => new Weekday(7) )}
 
+  // Absolute Month
+  def Month: Rule1[Month]     = rule {January | Febuary | March | April | May | June | July | August | September | October | November | December}
+  def January: Rule1[Month]   = rule {capture(ignoreCase("january")   | ignoreCase("jan") ) ~> ((s: String) => new Month(1) )}
+  def Febuary: Rule1[Month]   = rule {capture(ignoreCase("february")  | ignoreCase("feb") ) ~> ((s: String) => new Month(2) )}
+  def March: Rule1[Month]     = rule {capture(ignoreCase("march")     | ignoreCase("mar") ) ~> ((s: String) => new Month(3) )}
+  def April: Rule1[Month]     = rule {capture(ignoreCase("april")     | ignoreCase("apr") ) ~> ((s: String) => new Month(4) )}
+  def May: Rule1[Month]       = rule {capture(ignoreCase("may"))                            ~> ((s: String) => new Month(5) )}
+  def June: Rule1[Month]      = rule {capture(ignoreCase("june")      | ignoreCase("jun") ) ~> ((s: String) => new Month(6) )}
+  def July: Rule1[Month]      = rule {capture(ignoreCase("july")      | ignoreCase("jul") ) ~> ((s: String) => new Month(7) )}
+  def August: Rule1[Month]    = rule {capture(ignoreCase("august")    | ignoreCase("aug") ) ~> ((s: String) => new Month(8) )}
+  def September: Rule1[Month] = rule {capture(ignoreCase("september") | ignoreCase("sep") ) ~> ((s: String) => new Month(9) )}
+  def October: Rule1[Month]   = rule {capture(ignoreCase("october")   | ignoreCase("oct") ) ~> ((s: String) => new Month(10) )}
+  def November: Rule1[Month]  = rule {capture(ignoreCase("november")  | ignoreCase("nov") ) ~> ((s: String) => new Month(11) )}
+  def December: Rule1[Month]  = rule {capture(ignoreCase("december")  | ignoreCase("dec") ) ~> ((s: String) => new Month(12) )}
+
+
   def Space = rule { zeroOrMore(" ") }
 }
 
@@ -77,4 +96,11 @@ case class Weekday(d: Int) {
   require(d >= 1 && d <= 7)
 }
 
-case class Config(weekday: Option[Weekday])
+case class Month(m: Int) {
+  require(m >= 1 && m <= 12)
+}
+
+case class Config(
+  month: Option[Month] = None,
+  weekday: Option[Weekday] = None
+)
