4de613822d46a88913a2ba4248f6ada906ca06f5
Oliver Schrenk <oliver.schrenk@gmail.com>
2015-02-07 15:32:20 +0100

    Update all the versions

--- project/Build.scala
+++ project/Build.scala
@@ -6,17 +6,17 @@ object BuildSettings {
     name         := "datr.scala",
     version      := "0.1",
     organization := "platzhaltr",
-    scalaVersion := "2.11.4",
+    scalaVersion := "2.11.5",
     scalacOptions ++= Seq()
   )
 }
 
 object Dependencies {
-  val jodaTime    = "joda-time" % "joda-time" % "2.6"
+  val jodaTime    = "joda-time" % "joda-time" % "2.7"
   val jodaConvert = "org.joda" % "joda-convert" % "1.7"
-  val scalatest   = "org.scalatest" %% "scalatest" % "2.2.1" % "test"
+  val scalatest   = "org.scalatest" %% "scalatest" % "2.2.4" % "test"
     val parboiled   = "org.parboiled" %% "parboiled" % "2.0.1"
-  val reflect     = "org.scala-lang" % "scala-reflect" % "2.11.4"
+  val reflect     = "org.scala-lang" % "scala-reflect" % "2.11.5"
 
   val myDependencies = Seq(jodaTime, jodaConvert, scalatest, parboiled, reflect)
 }
