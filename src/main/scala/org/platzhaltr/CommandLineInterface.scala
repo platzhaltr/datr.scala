506460a0288c474325c12bc3d03d9936e583d326
Oliver Schrenk <oliver.schrenk@gmail.com>
2015-04-12 21:37:43 +0200

    Move formatting into new method

--- src/main/scala/org/platzhaltr/CommandLineInterface.scala
+++ src/main/scala/org/platzhaltr/CommandLineInterface.scala
@@ -8,6 +8,8 @@ import org.joda.time.{LocalDate,LocalDateTime}
 import org.parboiled2.{ParseError, ErrorFormatter}
 
 object CommandLineInterface extends App {
+  val errorFormatter = new ErrorFormatter(showTraces = true)
+
   repl()
 
   @tailrec
@@ -17,25 +19,29 @@ object CommandLineInterface extends App {
       case line =>
         val parser = new DateParser(line)
         parser.InputLine.run() match {
-          case Success(result) =>
-            result match {
-              case Left(Left(dateEvent)) =>
-                val today = new LocalDate
-                println(s"Result: ${dateEvent.process(today)}")
-              case Left(Right(timeEvent)) =>
-                val now = new LocalDateTime
-                println(s"Result: ${timeEvent.process(now)}")
-              case Right(Left(dateDuration)) =>
-                val today = new LocalDate
-                println(s"Result: ${dateDuration.process(today)}")
-              case Right(Right(timeDuration)) =>
-                val now = new LocalDateTime
-                println(s"Result: ${timeDuration.process(now)}")
-            }
-          case Failure(e: ParseError) => println(s"Invalid expression: ${parser.formatError(e, new ErrorFormatter(showTraces = true))}")
+          case Success(result) => println(s"Result: ${format(result)}")
+          case Failure(e: ParseError) =>
+            println(s"Invalid expression: ${parser.formatError(e, errorFormatter)}")
           case Failure(e)             => println(s"Unexpected error: ${e}")
         }
         repl()
+    }
+  }
+
+  def format(result: ParsedCompound): String = {
+    result match {
+      case Left(Left(dateEvent)) =>
+        val today = new LocalDate
+        s"${dateEvent.process(today)}"
+      case Left(Right(timeEvent)) =>
+        val now = new LocalDateTime
+        s"${timeEvent.process(now)}"
+      case Right(Left(dateDuration)) =>
+        val today = new LocalDate
+        s"${dateDuration.process(today)}"
+      case Right(Right(timeDuration)) =>
+        val now = new LocalDateTime
+        s"${timeDuration.process(now)}"
     }
   }
 }
